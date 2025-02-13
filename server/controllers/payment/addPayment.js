fa670de2a0eb9b97757c7244626ab42f14de34b0
sasith duleepa <duleepajayathissa@gmail.com>
2025-02-14 00:50:30 +0530

    payment

--- server/controllers/payment/addPayment.js
+++ server/controllers/payment/addPayment.js
@@ -1,30 +1,42 @@
-const DB = require('../../config/dataBase')
+const DB = require('../../config/dataBase');
 
-const AddPayment = (req,res) => {
-    console.log(req.body)
-
-    const { customer_id, payment} = req.body;
-    
-    if(payment.length > 0){
-        payment.forEach(element => {
-            DB.connection.query(`INSERT INTO payment (credit_id, payment_amount, payment_date, payment_user_id, payment_status) VALUES (${element._id}, ${element.payment}, '${req.date}' , '${req.user}', 'ok')`, (err, result) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(result)
-                }
-            })
-
-            DB.connection.query(`UPDATE credit SET payable_amount = payable_amount- ${element.payment} WHERE _id = ${element._id}`, (err, result) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(result)
-                }
-            })
+const executeQuery = (query, values = []) => {
+    return new Promise((resolve, reject) => {
+        DB.connection.query(query, values, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
         });
-    }
-
-
+    });
 };
+
+const AddPayment = async (req, res) => {
+    try {
+        console.log(req.body);
+
+        const { customer_id, payment } = req.body;
+        const payment_date = req.date; // Assuming req.date is correctly set
+        const payment_user_id = req.user; // Assuming req.user is correctly set
+
+
+        if(payment >0){
+            // Insert payment record
+        const insertQuery = `INSERT INTO payment (payment_amount, payment_date, payment_user_id) VALUES (?, ?, ?)`;
+        await executeQuery(insertQuery, [payment, payment_date, payment_user_id]);
+
+        // Update credit table
+        const updateQuery = `UPDATE customers SET total_payable = total_payable - ? WHERE customer_id = ?`;
+        await executeQuery(updateQuery, [payment, customer_id]);
+
+        res.status(200).json({ message: "Payment added successfully" });
+        }else{
+            res.status(204).json({ message: "check payment amount" });
+        }
+
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error processing payment", error });
+    }
+};
+
 module.exports = AddPayment;
